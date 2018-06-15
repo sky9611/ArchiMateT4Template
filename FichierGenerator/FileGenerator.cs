8f283a04df9bcf0a4fe56550b6eb40a2857e2114
Unknown <collinbeyond@gmail.com>
2018-06-15 11:22:24 +0200

    add new funcs
    - static
    + constructor
    + getAllGroup
    + getAllView

--- FichierGenerator/FileGenerator.cs
+++ FichierGenerator/FileGenerator.cs
@@ -1,10 +1,24 @@
 using FichierGenerator.Template;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace FichierGenerator
 {
-    public class Program
+    public class FileGenerator
     {
+        string file_path;
+        XElement doc;
+        XNamespace NP;
+        XNamespace xmlns_xsi = "http://www.w3.org/2001/XMLSchema-instance";
+
+        public FileGenerator(string file_path)
+        {
+            this.file_path = file_path;
+            doc = XElement.Load(file_path);
+            NP = doc.GetDefaultNamespace();
+        }
+
         public static readonly string[] all_types = {
             "BusinessActor",
             "BusinessCollaboration",
@@ -28,16 +42,21 @@ namespace FichierGenerator
             "ApplicationComponent",
             "ApplicationEvent",
             "ApplicationProcess"};
+
+        public string File_path { get => file_path; set => file_path = value; }
+
         /// <summary>
         ///     Method to run the t4 template
         /// </summary>
-        /// <param name="input_name"> The name of xml fichier which describe the architecture</param>
         /// <param name="output_name"> The name of the fichier created</param>
-        public static void Generate(string input_name, string output_name, string[] types = null, string[] groups = null, string[] views = null)
+        /// <param name="types"> The selected types </param>
+        /// <param name="groups"> The selected groups </param>
+        /// <param name="views"> The selected views </param>
+        public void Generate(string output_name, string[] types = null, string[] groups = null, string[] views = null)
         {
             var generator = new Generator();
             generator.Session = new Microsoft.VisualStudio.TextTemplating.TextTemplatingSession();
-            generator.Session["input_name"] = input_name;
+            generator.Session["input_name"] = file_path;
             generator.Session["groups"] = groups;
             generator.Session["views"] = views;
             if (types==null)
@@ -58,6 +77,33 @@ namespace FichierGenerator
             else
                 path_log = "GenerationLog.txt";
             System.IO.File.WriteAllText(path_log, log);
+        }
+
+        public string[] getAllType() { return all_types; }
+
+        public string[] getAllGroup()
+        {
+            IEnumerable<XElement> xeles_group = from e in doc.Descendants(NP + "element")
+                                                where e.Attribute(xmlns_xsi + "type").Value == "Grouping"
+                                                select e;
+            List<string> list_group = new List<string>();
+            foreach (var ele in xeles_group)
+            {
+                list_group.Add(ele.Element(NP + "name").Value);
+            }
+            return list_group.ToArray();
+        }
+
+        public string[] getAllView()
+        {
+            IEnumerable<XElement> xeles_view = from e in doc.Descendants(NP + "view")
+                                               select e;
+            List<string> list_view = new List<string>();
+            foreach (var ele in xeles_view)
+            {
+                list_view.Add(ele.Element(NP + "name").Value);
+            }
+            return list_view.ToArray();
         }
 
         //static void Main(string[] args)
